1aa33886acf6dfd9186c48ba4e172856a3fa20fb
Brúnó Bánóczi-Csernák <bruno_bano@Bandolero.local>
2024-09-19 09:22:35 +0200

    Initialize steps branch

--- main.js
+++ main.js
@@ -1,37 +1,8 @@
-// function flipCard() {
-//   document.querySelector(".left").classList.toggle("flipped-left");
-//   document.querySelector(".right").classList.toggle("flipped-right");
-// }
 console.log("kurva");
 const brochure = document.querySelector(".brochure");
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
 let stage = 0;
-
-// Add an event listener to the whole document for the keydown event
-// document.addEventListener("keydown", function (event) {
-//   switch (event.key) {
-//     case "ArrowUp":
-//       updateBrochure();
-//       console.log("anyad");
-//       stage++;
-//       break;
-//     case "ArrowDown":
-//       output.textContent = "You pressed the DOWN arrow!";
-//       handleDownArrow();
-//       break;
-//     case "ArrowLeft":
-//       handleLeftArrow();
-//       break;
-//     case "ArrowRight":
-//       output.textContent = "You pressed the RIGHT arrow!";
-//       handleRightArrow();
-//       break;
-//     default:
-//       output.textContent = "Press one of the arrow keys!";
-//       break;
-//   }
-// });
 
 nextButton.addEventListener("click", () => {
   if (stage < 2) {
