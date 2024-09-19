b62e22504437564fd1b76c0d1a2297bb1e9f68ae
Brúnó Bánóczi-Csernák <bruno.banoczi@gmail.com>
2024-09-19 12:47:11 +0200

    Making the differents steps to interactively open and close the brochure pages

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const brochure = document.querySelector(".brochure");
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
 let stage = 0;
+let step = "";
 
 nextButton.addEventListener("click", () => {
   if (stage < 2) {
     stage++;
+    step = "next";
     updateBrochure();
   }
 });
@@ -14,6 +16,7 @@ nextButton.addEventListener("click", () => {
 prevButton.addEventListener("click", () => {
   if (stage > 0) {
     stage--;
+    step = "prev";
     updateBrochure();
   }
 });
@@ -24,16 +27,33 @@ function updateBrochure() {
       // brochure.style.transform = "rotateY(0deg)";
       document.querySelector(".left").classList.remove("flipped-left");
       document.querySelector(".right").classList.remove("flipped-right");
+
       console.log("stage0");
       break;
     case 1:
-      document.querySelector(".left").classList.toggle("flipped-left");
-      document.querySelector(".right").classList.toggle("flipped-right");
-      console.log("stage1");
+      if (step === "next") {
+        document.querySelector(".left").classList.toggle("flipped-left");
+        document.querySelector(".right").classList.toggle("flipped-right");
+        console.log("stage1");
+      } else {
+        setTimeout(() => {
+          document.querySelector(".brochure").classList.remove("flipped");
+          document.querySelector(".middle-flip").classList.remove("top");
+        }, 1000); // 7000 milliseconds = 3 seconds
+        document.querySelector(".left").classList.toggle("flipped-left");
+        document.querySelector(".right").classList.toggle("flipped-right");
+      }
+
       break;
     case 2:
       // brochure.style.transform = "rotateY(-180deg)";
       document.querySelector(".brochure").classList.toggle("flipped");
+      setTimeout(() => {
+        document.querySelector(".left").classList.remove("flipped-left");
+        document.querySelector(".right").classList.remove("flipped-right");
+      }, 1000); // 7000 milliseconds = 3 seconds
+      document.querySelector(".middle-flip").classList.toggle("top");
+
       console.log("stage2");
       break;
   }
