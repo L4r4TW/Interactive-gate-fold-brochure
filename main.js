136bc42e451beb0f7722df3c52d70f12c6323fa8
Brúnó Bánóczi-Csernák <bruno.banoczi@gmail.com>
2024-09-26 16:56:30 +0200

    WIP: Temporary commit

--- main.js
+++ main.js
@@ -24,33 +24,40 @@ prevButton.addEventListener("click", () => {
 function updateBrochure() {
   switch (stage) {
     case 0:
-      // brochure.style.transform = "rotateY(0deg)";
-      document.querySelector(".left").classList.remove("flipped-left");
-      document.querySelector(".right").classList.remove("flipped-right");
+      document.querySelector(".left").style.transform = "rotateY(180deg)";
+      document.querySelector(".right").style.transform = "rotateY(-180deg)";
 
       console.log("stage0");
       break;
     case 1:
       if (step === "next") {
-        document.querySelector(".left").classList.toggle("flipped-left");
-        document.querySelector(".right").classList.toggle("flipped-right");
+        // document.querySelector(".left").classList.toggle("flipped-left");
+        document.querySelector(".left").style.transform = "rotateY(0deg)";
+        // document.querySelector(".right").classList.toggle("flipped-right");
+        document.querySelector(".right").style.transform = "rotateY(0deg)";
         console.log("stage1");
       } else {
         setTimeout(() => {
-          document.querySelector(".brochure").classList.remove("flipped");
-          document.querySelector(".middle-flip").classList.remove("top");
+          // document.querySelector(".brochure").classList.remove("flipped");
+          document.querySelector(".flipped").style.transform = "rotateY(0deg)";
+          // document.querySelector(".middle-flip").classList.remove("top");
         }, 1000); // 7000 milliseconds = 3 seconds
-        document.querySelector(".left").classList.toggle("flipped-left");
-        document.querySelector(".right").classList.toggle("flipped-right");
+        // document.querySelector(".left").classList.toggle("flipped-left");
+        document.querySelector(".left").style.transform = "rotateY(0deg)";
+        // document.querySelector(".right").classList.toggle("flipped-right");
+        document.querySelector(".right").style.transform = "rotateY(0deg)";
       }
 
       break;
     case 2:
       // brochure.style.transform = "rotateY(-180deg)";
       document.querySelector(".brochure").classList.toggle("flipped");
+      // document.querySelector(".middle-flip").style.zIndex = "1000";
       setTimeout(() => {
-        document.querySelector(".left").classList.remove("flipped-left");
-        document.querySelector(".right").classList.remove("flipped-right");
+        // document.querySelector(".left").classList.remove("flipped-left");
+        document.querySelector(".left").style.transform = "rotateY(-180deg)";
+        // document.querySelector(".right").classList.remove("flipped-right");
+        document.querySelector(".right").style.transform = "rotateY(180deg)";
       }, 1000); // 7000 milliseconds = 3 seconds
       document.querySelector(".middle-flip").classList.toggle("top");
 
